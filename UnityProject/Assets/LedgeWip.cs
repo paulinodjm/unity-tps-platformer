e7ae0b5de3efb8dcbaae1abdf76963a9a77c29a0
Paulino <paulinodjm@hotmail.com>
2018-03-09 16:47:54 +0100

    Add the multi-ledge handling

--- UnityProject/Assets/LedgeWip.cs
+++ UnityProject/Assets/LedgeWip.cs
@@ -5,6 +5,34 @@ using UnityEngine;
 [ExecuteInEditMode()]
 public class LedgeWip : MonoBehaviour {
 
+  [System.Serializable]
+  public struct Ledge {
+
+    [Tooltip("The first ledge handle")]
+    public Transform handle0;
+
+    [Tooltip("The second ledge handle")]
+    public Transform handle1;
+
+    /// <summary>
+    /// Returns the ledge vector.
+    /// </summary>
+    public Vector3 LedgeVector {
+      get {
+        return handle0 != null && handle1 != null ? handle1.position - handle0.position : Vector3.zero;
+      }
+    }
+
+    /// <summary>
+    /// Returns the vector pointing toward the floor side of the ledge.
+    /// </summary>
+    public Vector3 FloorVector {
+      get {
+        return Vector3.Cross(LedgeVector, Vector3.up).normalized;
+      }
+    }
+  }
+
   [Header("Gizmos")]
   [Tooltip("The ledge line color")]
   public Color ledgeColor = Color.blue;
@@ -22,73 +50,58 @@ public class LedgeWip : MonoBehaviour {
   public Color crossExtendsColor = Color.red;
 
   [Header("Objects")]
-  [Tooltip("The first ledge handle")]
-  public Transform Ledge0;
-
-  [Tooltip("The second ledge handle")]
-  public Transform Ledge1;
+  public Ledge[] ledges = new Ledge[2];
 
   [Tooltip("The object that mimics a player")]
   public Transform needle;
 
-  /// Returns the ledge vector
-  public Vector3 LedgeVector {
-    get {
-      return Ledge0 == null || Ledge1 == null ? Vector3.zero : Ledge1.position - Ledge0.position;
-    }
-  }
-
-  public Vector3 FloorVector {
-    get {
-      return Vector3.Cross(LedgeVector, Vector3.up).normalized;
-    }
-  }
-
   void OnDrawGizmos() {
-    DrawExtendsGizmos();
-    DrawNeedleGizmos();
+    foreach (var ledge in ledges) {
+      DrawExtendsGizmos(ledge);
+      DrawNeedleGizmos(ledge);
+    }
   }
 
   void Update() {
   }
 
-  private void DrawExtendsGizmos() {
-    if (Ledge0 == null || Ledge1 == null) return;
+  private void DrawExtendsGizmos(Ledge ledge) {
+    if (ledge.handle0 == null || ledge.handle1 == null) return;
 
     Gizmos.color = ledgeColor;
 
     // draws the ledge line
-    Gizmos.DrawRay(Ledge0.position, LedgeVector);
+    Gizmos.DrawRay(ledge.handle0.position, ledge.LedgeVector);
 
     // draws rays at ledge_0
-    Gizmos.DrawRay(Ledge0.position, -Vector3.up * ledgeExtendsSize);
-    Gizmos.DrawRay(Ledge0.position, FloorVector * ledgeExtendsSize);
+    Gizmos.DrawRay(ledge.handle0.position, -Vector3.up * ledgeExtendsSize);
+    Gizmos.DrawRay(ledge.handle0.position, ledge.FloorVector * ledgeExtendsSize);
 
     // draws rays at ledge_1
-    Gizmos.DrawRay(Ledge1.position, -Vector3.up * ledgeExtendsSize);
-    Gizmos.DrawRay(Ledge1.position, FloorVector * ledgeExtendsSize);
+    Gizmos.DrawRay(ledge.handle1.position, -Vector3.up * ledgeExtendsSize);
+    Gizmos.DrawRay(ledge.handle1.position, ledge.FloorVector * ledgeExtendsSize);
   }
 
-  private void DrawNeedleGizmos() {
-    if (needle == null || Ledge0 == null || Ledge1 == null) return;
+  private void DrawNeedleGizmos(Ledge ledge) {
+    if (needle == null || ledge.handle0 == null || ledge.handle1 == null) return;
 
     // find the nearest point on the ledge vector
-    var ledgeNormal = LedgeVector.normalized;
-    var needleOnNormalized = needle.position - Ledge0.position;
+    var ledgeNormal = ledge.LedgeVector.normalized;
+    var needleOnNormalized = needle.position - ledge.handle0.position;
     var projectedNormalized = Vector3.Project(needleOnNormalized, ledgeNormal);
-    var res_nearestLedgePoint = Ledge0.position + projectedNormalized;
+    var res_nearestLedgePoint = ledge.handle0.position + projectedNormalized;
 
     // find whether the point is really on the ledge or on the extends only
-    var pointFromLedge0 = res_nearestLedgePoint - Ledge0.position;
-    var pointFromLedge1 = res_nearestLedgePoint - Ledge1.position;
+    var pointFromLedge0 = res_nearestLedgePoint - ledge.handle0.position;
+    var pointFromLedge1 = res_nearestLedgePoint - ledge.handle1.position;
     var magnitudeFromLedge0 = Vector3.SqrMagnitude(pointFromLedge0);
     var magnitudeFromLedge1 = Vector3.SqrMagnitude(pointFromLedge1);
-    var res_isLedge0Extends = magnitudeFromLedge0 > LedgeVector.sqrMagnitude;
-    var res_isLedge1Extends = magnitudeFromLedge1 > LedgeVector.sqrMagnitude;
+    var res_isLedge0Extends = magnitudeFromLedge0 > ledge.LedgeVector.sqrMagnitude;
+    var res_isLedge1Extends = magnitudeFromLedge1 > ledge.LedgeVector.sqrMagnitude;
     var res_isLedgeExtends = res_isLedge0Extends || res_isLedge1Extends;
 
     // find whether the character is on the ground side of the ledge or not
-    var res_isGrounded = Vector3.Dot(FloorVector, needleOnNormalized) >= 0;
+    var res_isGrounded = Vector3.Dot(ledge.FloorVector, needleOnNormalized) >= 0;
 
 
     // draws the grab ray
